d940581e863ccaefedcf347d929ee06af7caed31
Phạm Anh Quân <Phamquan2112004@gmail.com>
2025-10-31 21:26:18 +0700

    Init Project

---
+++ services/todo_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/haiquanbg1/golang-todo-app/repositories"
+)
+
+type TodoService interface {
+	Demo() string
+}
+
+type todoService struct {
+	repo repositories.TodoRepository
+}
+
+func NewTodoService(repo repositories.TodoRepository) TodoService {
+	return &todoService{
+		repo: repo,
+	}
+}
+
+func (service *todoService) Demo() string {
+	return service.repo.Demo()
+}
