759b4fde7bface81ddc048be237e0f268d49a6d8
Phạm Anh Quân <Phamquan2112004@gmail.com>
2025-11-11 10:01:53 +0700

    Setup Environment

--- config/db.go
+++ config/db.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect(dsn string) {
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	DB = database
+}
